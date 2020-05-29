d4572dc6919dadb13d20ef604956a7ded3ff3d5f
pasteljoc <jrojas.tareas@gmail.com>
2020-05-29 15:01:07 -0400

    agrega corrección a la hora del sistema para que sea utc-4

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-from datetime import datetime, date
-from datetime import date
+from datetime import datetime, date, timedelta
 
 import sqlite3
 from sqlite3 import Error
@@ -32,7 +31,8 @@ async def hola(ctx):
 
 @client.command()
 async def hora(ctx):
-    await ctx.send(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    horaChile=datetime.now()-timedelta(hours=4)
+    await ctx.send(horaChile.strftime('%Y-%m-%d %H:%M:%S'))
 
 @client.command()
 async def onomastico(ctx):
